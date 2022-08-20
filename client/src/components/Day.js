1f8784f1fa328699e41439466a68daef4bf2932b
CleveyP <clevelandplonsey@gmail.com>
2022-08-20 00:36:18 -0700

    add documentation to Day component

--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -3,17 +3,15 @@ import DayEvent from './DayEvent';
 
 const Day = (props) => {
     let [eventList, setEventList] = useState([]);
-    const createEvent = () =>{
-        var eventName = prompt("Input the name of the event you wish to create"); //wait for user to name event and then add it to the state
-        var eventToBeAdded = {title: eventName, key: eventList.length};
-        setEventList([...eventList, eventToBeAdded]); //add it to the state
+    const createEvent = () =>{ //creates a dayEvent object and updates the state to hold it.
+        let eventName = prompt("Input the name of the event you wish to create"); //wait for user to name event and store it to eventName
+        let eventToBeAdded = {title: eventName, key: eventList.length}; //give each DayEvent object a key based on the length of the current list
+        setEventList([...eventList, eventToBeAdded]); //add the new event to the state array
     }
 
-    const deleteEvent = (e, key) =>{
-      
-        console.log(key);
-        let newEventList = eventList.filter(element => element.key!==key);
-        setEventList(newEventList);
+    const deleteEvent = (e, key) =>{ //deletes a dayevent object from the state array
+        let newEventList = eventList.filter(element => element.key!==key); //filter out of eventList the event to be deleted by key
+        setEventList(newEventList); //reset the state of the Day component to echo the changes to the event list
     }
 
     return (
