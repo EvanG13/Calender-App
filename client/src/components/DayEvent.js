d1d8a80c98e0b030039b835af8c2cd74c7d0b53e
CleveyP <clevelandplonsey@gmail.com>
2022-08-19 21:04:10 -0700

    implement DayEvent component

---
+++ client/src/components/DayEvent.js
@@ -0,0 +1,18 @@
+
+
+const DayEvent = (props) =>{
+    return (
+        <div className='day-event'>
+            <p className='day-event-title'>
+                {props.eventTitle}
+            </p>
+            <button className='delete-day-event'
+            onClick={props.handleClick}>
+                X
+            </button>
+        </div>
+        
+    )
+}
+
+export default DayEvent;
