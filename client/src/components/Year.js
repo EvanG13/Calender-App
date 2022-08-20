4c3692627640860e65c5bcc3f355f90c00b2f94a
CleveyP <clevelandplonsey@gmail.com>
2022-08-20 05:29:29 -0700

    drill all the dates through all components

--- client/src/components/Year.js
+++ client/src/components/Year.js
@@ -11,18 +11,18 @@ let febNumDays = (leapYear(props.yearNum)) ? 29 : 28;
     return (
         <div className="Year">
             <h2>{props.year}</h2>
-            <Month name="January" numDays={31}/>
-            <Month name="February" numDays={febNumDays}/>
-            <Month name="March" numDays={31}/>
-            <Month name="April" numDays={30}/>
-            <Month name="May" numDays={31}/>
-            <Month name="June" numDays={30}/>
-            <Month name="July" numDays={31}/>
-            <Month name="August" numDays={31}/>
-            <Month name="September" numDays={30}/>
-            <Month name="October" numDays={31}/>
-            <Month name="November" numDays={30}/>
-            <Month name="December" numDays={31}/>
+            <Month year={props.yearNum} month={1} name="January" numDays={31} />
+            <Month year={props.yearNum} month={2} name="February" numDays={febNumDays}/>
+            <Month year={props.yearNum} month={3} name="March" numDays={31}/>
+            <Month year={props.yearNum} month={4} name="April" numDays={30}/>
+            <Month year={props.yearNum} month={5} name="May" numDays={31}/>
+            <Month year={props.yearNum} month={6} name="June" numDays={30}/>
+            <Month year={props.yearNum} month={7} name="July" numDays={31}/>
+            <Month year={props.yearNum} month={8} name="August" numDays={31}/>
+            <Month year={props.yearNum} month={9} name="September" numDays={30}/>
+            <Month year={props.yearNum} month={10} name="October" numDays={31}/>
+            <Month year={props.yearNum} month={11} name="November" numDays={30}/>
+            <Month year={props.yearNum} month={12} name="December" numDays={31}/>
         </div> 
     );
 }
---
+++ client/src/helperFunctions/dateFunctions.js
@@ -0,0 +1,48 @@
+
+
+export const dateIsPassed = (date) =>{ //takes a date object {year: . month: , day: , } and compares it with the current date returns false if the arg is bigger than the current date
+    const currentDate = new Date();
+    const dateObj = new Date(date.year, date.month, date.day);
+    return ((currentDate.getTime() - dateObj.getTime()) > 0)
+}
+
+export const isCurrentDate = (date) =>{
+    const currentDate = new Date();
+    const dateObj = new Date(date.year, date.month, date.day);
+    return currentDate === dateObj;
+}
+
+export const getDayOfWeek = (date) =>{
+    const dateObj = new Date(date.year, date.month, date.day);
+    console.log(date.year, date.month, date.day);
+    let dayOfWeek;
+    switch(dateObj.getDay()){
+        case 3:
+            dayOfWeek = 'Sunday';
+            break;
+        case 4:
+            dayOfWeek = 'Monday';
+            break;
+        case 5:
+            dayOfWeek = 'Tuesday';
+            break;
+        case 6:
+            dayOfWeek = 'Wednesday';
+            break;
+        case 0:
+            dayOfWeek = 'Thursday';
+            break; 
+        case 1:
+            dayOfWeek = 'Friday';
+            break;
+        case 2:
+            dayOfWeek = 'Saturday';
+            break;
+        default:
+            console.log("problem getting day of week.");
+            console.log("recieved:" + dateObj.getDay());
+    }
+    return dayOfWeek;
+}
+
+
