f6004303e0816a2837ff9974a86c260c188651de
CleveyP <clevelandplonsey@gmail.com>
2022-08-19 21:03:34 -0700

    implement Day component

--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -1,7 +1,28 @@
+import {React, useState} from 'react';
+import DayEvent from './DayEvent';  
 
 const Day = (props) => {
+    let [eventList, setEventList] = useState([]);
+    const createEvent = () =>{
+        var eventName = prompt("Input the name of the event you wish to create"); //wait for user to name event and then add it to the state
+        var eventToBeAdded = {title: eventName, key: eventList.length};
+        setEventList([...eventList, eventToBeAdded]); //add it to the state
+    }
+
+    const deleteEvent = (e, key) =>{
+      
+        console.log(key);
+        let newEventList = eventList.filter(element => element.key!==key);
+        setEventList(newEventList);
+    }
+
     return (
-        <p>dayNum, {props.dayNum}</p>
+        <div className="day-container">
+            <p> {props.dayNum}</p>
+            {eventList.map(
+                (element, index) => <DayEvent handleClick = {() => {deleteEvent(element, element.key)}} eventTitle = {element.title} key={element.key}/>)}
+            <button onClick={createEvent}>Create Event</button>
+        </div>
     );
 }
 
