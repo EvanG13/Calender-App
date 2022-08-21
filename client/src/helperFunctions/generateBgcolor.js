9a71a599310028589235867eddaa77545cf9beb1
CleveyP <clevelandplonsey@gmail.com>
2022-08-21 01:04:01 -0700

    modify generateBgColor function

--- client/src/helperFunctions/generateBgcolor.js
+++ client/src/helperFunctions/generateBgcolor.js
@@ -5,7 +5,7 @@ const generateBgColor = (dayOfWeek, isPassed, isCurrentDate) =>{
         bgColor = {backgroundColor: "#161748", color: "white"};
     }
     if(isPassed){
-        bgColor = {backgroundColor: "#bc7a94" , color: "black"};
+        bgColor = {backgroundColor: "#7c5665" , color: "black"};
     }
     if(isCurrentDate){
         bgColor = { backgroundColor: "yellow", color: "black"};
