396c4f9a58b038dec8eca6f717ec0fbae7a5ad38
CleveyP <clevelandplonsey@gmail.com>
2022-08-21 00:34:28 -0700

    fix generateBgColor

--- client/src/helperFunctions/generateBgcolor.js
+++ client/src/helperFunctions/generateBgcolor.js
@@ -1,12 +1,16 @@
 
-const generateBgColor = (dayOfWeek, isPassed) =>{
-    let bgColor;
+const generateBgColor = (dayOfWeek, isPassed, isCurrentDate) =>{
+    let bgColor = {};
     if(dayOfWeek === "Saturday" || dayOfWeek === "Sunday"){
         bgColor = {backgroundColor: "#161748", color: "white"};
     }
     if(isPassed){
-        bgColor = {backgroundColor: "gray", color: "black"};
+        bgColor = {backgroundColor: "#bc7a94" , color: "black"};
     }
+    if(isCurrentDate){
+        bgColor = { backgroundColor: "yellow", color: "black"};
+    }
+
     return bgColor;
 }
 
